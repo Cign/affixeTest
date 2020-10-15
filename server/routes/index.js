96e2052e470629e7f9a7074ae58743972a9aea20
y <>
2020-10-15 18:57:14 +0200

    initialization

---
+++ server/routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.render('index', { title: 'ExpressTest' });
+});
+
+module.exports = router;
---
+++ server/routes/users.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.send('respond with a resource@@@');
+});
+
+module.exports = router;
